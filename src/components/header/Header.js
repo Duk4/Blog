233178969085f9fa274e5e745deaa676ccc2691d
Dule <crashendodt@gmail.com>
2019-09-03 16:02:10 +0200

    Added a header component and some basic css

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="welcome">
+                <h1>Dobrodošli</h1>
+                <p>na moj skromni blog, gde ću sa vama podeliti svoja iskustva iz IT industrije.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
