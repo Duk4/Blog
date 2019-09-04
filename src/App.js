63a7416118ce8471fc927498484a6959edafe6f0
Dule <crashendodt@gmail.com>
2019-09-04 17:43:33 +0200

    Designed Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div>
       <Header />
+      <Navbar />
       <Footer />
     </div>
   );
