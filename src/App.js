006bce025fdb58ffa9ad947b190927e6b88159b2
Dule <crashendodt@gmail.com>
2019-09-04 16:56:24 +0200

    Created and displayed a footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>&copy; Dušan Tanasić</p>
+        </div>
+    );
+};
+
+export default Footer;
