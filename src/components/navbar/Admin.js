af31ac84e4bed4a32e3528fb6aadf2b1291ddc41
Dule <crashendodt@gmail.com>
2019-09-07 20:15:00 +0200

    Additional files created

--- src/components/navbar/Admin.js
+++ src/components/navbar/Admin.js
@@ -1,17 +1,14 @@
 import React from "react";
 
-function Navbar() {
+function Admin() {
     return (
         <div className="container">
             <div className="navbar">
                 <a href={"https://github.com/Duk4"}>Blog</a>
-                <a href={"https://github.com/Duk4"}>O meni</a>
-                <a href={"https://github.com/Duk4"}>GitHub</a>
+                <a href={"https://github.com/Duk4"}>+</a>
             </div>
         </div>
     );
 }
 
-export default Navbar;
-
-// change this...
+export default Admin;
--- src/components/navbar/Guest.js
+++ src/components/navbar/Guest.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Navbar() {
+function Guest() {
     return (
         <div className="container">
             <div className="navbar">
@@ -12,4 +12,4 @@ function Navbar() {
     );
 }
 
-export default Navbar;
+export default Guest;
